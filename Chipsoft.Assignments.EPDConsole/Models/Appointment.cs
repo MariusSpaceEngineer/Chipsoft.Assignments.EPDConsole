a418e2ca3e3d3f91ddf4520eaee780d5345c5066
Marius Stoica <stoicamarius9010@gmail.com>
2025-02-03 23:29:06 +0100

    Add navigation properties to Appointment model

--- Chipsoft.Assignments.EPDConsole/Models/Appointment.cs
+++ Chipsoft.Assignments.EPDConsole/Models/Appointment.cs
@@ -4,7 +4,9 @@
     {
         public int Id { get; set; }
         public int PatientId { get; set; }
+        public Patient Patient { get; set; }
         public int PhysicianId { get; set; }
+        public Physician Physician { get; set; }
         public DateTime Date { get; set; }
         public string Description { get; set; }
     }
