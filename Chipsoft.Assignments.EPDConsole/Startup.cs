49e2e6dc01383abfba9dd28a2252fc379883a926
Marius Stoica <stoicamarius9010@gmail.com>
2025-02-03 23:34:54 +0100

    Add Startup.cs

---
+++ Chipsoft.Assignments.EPDConsole/Startup.cs
@@ -0,0 +1,24 @@
+﻿using Chipsoft.Assignments.EPDConsole.Repositories;
+using Chipsoft.Assignments.EPDConsole.Repositories.Interfaces;
+using Chipsoft.Assignments.EPDConsole.Services;
+using Chipsoft.Assignments.EPDConsole.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Chipsoft.Assignments.EPDConsole
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<EPDDbContext>(options => options.UseSqlite("Data Source=epd.db"));
+            services.AddScoped<IAppointmentRespository, AppointmentRepository>();
+            services.AddScoped<IAppointmentService, AppointmentService>();
+            services.AddScoped<IPatientRepository, PatientRepository>();
+            services.AddScoped<IPatientService, PatientService>();
+            services.AddScoped<IPhysicianRepository, PhysicianRepository>();
+            services.AddScoped<IPhysicianService, PhysicianService>();
+            services.AddTransient<Program>();
+        }
+    }
+}
